9718a08ecc8e60a0e1c3ff8b8ad70739314974a6
Fred Cox <frederick.j.cox@gmail.com>
2017-11-03 15:46:31 +0200

    Add class name service id

--- tests/Mcfedr/BeanstalkQueueDriverBundle/Tests/DependencyInjection/McfedrBeanstalkQueueDriverExtensionTest.php
+++ tests/Mcfedr/BeanstalkQueueDriverBundle/Tests/DependencyInjection/McfedrBeanstalkQueueDriverExtensionTest.php
@@ -2,6 +2,7 @@
 
 namespace Mcfedr\BeanstalkQueueDriverBundle\Tests\DependencyInjection;
 
+use Mcfedr\BeanstalkQueueDriverBundle\Manager\BeanstalkQueueManager;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class McfedrBeanstalkQueueDriverExtensionTest extends WebTestCase
@@ -9,6 +10,7 @@ class McfedrBeanstalkQueueDriverExtensionTest extends WebTestCase
     public function testConfiguration()
     {
         $client = static::createClient();
+        $this->assertTrue($client->getContainer()->has(BeanstalkQueueManager::class));
         $this->assertTrue($client->getContainer()->has('mcfedr_queue_manager.default'));
     }
 }
